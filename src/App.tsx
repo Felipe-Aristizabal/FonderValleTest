732d6c87fc9fb0b5fa1946644429be49137badb8
Juan Pablo Martínez <jpablo.mguzman@gmail.com>
2025-05-16 20:15:30 -0500

    implement test data

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import { useEffect } from "react"
 import { Routes, Route } from "react-router-dom"
 import Layout from "./containers/layout"
 import HomePage from "./pages/home-page"
@@ -8,7 +9,26 @@ import VisitForm from "@/pages/visit-form"
 import NotFound from "./pages/not-found"
 
 function App() {
-  // localStorage.clear()
+  useEffect(() => {
+    const STORAGE_KEY = "evaluationFormData"
+
+    const loadInitialData = async () => {
+      const existing = localStorage.getItem(STORAGE_KEY)
+      if (!existing) {
+        try {
+          const res = await fetch("/example_users_data_negative.json")
+          const data = await res.json()
+          localStorage.setItem(STORAGE_KEY, JSON.stringify(data))
+          console.log("Datos precargados en localStorage")
+        } catch (error) {
+          console.error("Error cargando los datos iniciales:", error)
+        }
+      }
+    }
+
+    loadInitialData()
+  }, [])
+
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
