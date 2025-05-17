61601d1f4624f6145376eb43d6be693c71225fa1
Juan Pablo Martínez <jpablo.mguzman@gmail.com>
2025-05-16 20:37:12 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,21 +9,22 @@ import VisitForm from "@/pages/visit-form"
 import NotFound from "./pages/not-found"
 
 function App() {
-  // localStorage.clear()
   useEffect(() => {
     const STORAGE_KEY = "evaluationFormData"
 
     const loadInitialData = async () => {
-      const existing = localStorage.getItem(STORAGE_KEY)
-      if (!existing) {
-        try {
-          const res = await fetch("/example_users_data_negative.json")
-          const data = await res.json()
-          localStorage.setItem(STORAGE_KEY, JSON.stringify(data))
-          console.log("Datos precargados en localStorage")
-        } catch (error) {
-          console.error("Error cargando los datos iniciales:", error)
-        }
+      try {
+        // Siempre borrar primero
+        localStorage.removeItem(STORAGE_KEY)
+        console.log("Datos previos eliminados")
+
+        // Luego cargar nuevo archivo
+        const res = await fetch("/example_users_data_negative.json")
+        const data = await res.json()
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(data))
+        console.log("Datos precargados en localStorage")
+      } catch (error) {
+        console.error("Error cargando los datos iniciales:", error)
       }
     }
 
